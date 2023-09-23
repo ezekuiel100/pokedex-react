407eef5437caa6be210f90297a4e5fc69b6127fb
unknown <ezekuiel100@gmail.com>
2023-09-23 12:26:28 -0300

    third commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,22 @@
 import styles from "./Card.module.css";
-import Pokemon from "./Pokemon";
+
+import { useEffect, useState } from "react";
 
 function Card({ pokemons }) {
+  const [pokemon, setPoke] = useState(null);
+
+  useEffect(() => {
+    fetch(pokemons.url)
+      .then((res) => res.json())
+      .then((data) => setPoke(data));
+  }, [pokemons]);
+
+  if (pokemon == null) return;
+
   return (
     <div className={styles.card}>
-      <Pokemon pokemons={pokemons} />
+      <img src={pokemon.sprites.front_default} />
+      <p>{pokemon.name}</p>
     </div>
   );
 }
